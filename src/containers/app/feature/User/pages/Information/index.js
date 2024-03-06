d4ae297ae35d72e5e0a730291e03329602c52693
qd16092002 <tranquangdao16092002@gmail.com>
2024-03-06 17:43:11 +0700

    update code

--- src/containers/app/feature/User/pages/Information/index.js
+++ src/containers/app/feature/User/pages/Information/index.js
@@ -4,12 +4,21 @@ import { useDispatch, useSelector } from 'react-redux'
 import toast, { Toaster } from 'react-hot-toast'
 import { useForm } from 'react-hook-form'
 import { useChangeUserInformationMutation } from '../../userService'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { authApi } from '@src/containers/authentication/feature/Auth/authService'
 import { setUser } from '@src/containers/authentication/feature/Auth/authSlice'
 
 const cx = classNames.bind(styles)
+const TABS = {
+  MYINFOR: {
+    code: 'MYINFOR'
+  },
+  CHANGEPASSWORD: {
+    code: 'CHANGEPASSWORD'
+  }
+}
 function Information() {
+  const [activeTab, setActiveTab] = useState(TABS.MYINFOR)
   const { register, handleSubmit } = useForm()
   const formInput = useRef()
   const [updateUser, { isLoading: isUpdating }] = useChangeUserInformationMutation()
@@ -36,39 +45,61 @@ function Information() {
       <div className={cx('navbar')}>
         <div className={cx('title')}>My Info</div>
         <div className={cx('line')}></div>
+        <div className={cx('header__process')}>
+          <button
+            onClick={() => setActiveTab(TABS.MYINFOR)}
+            className={cx('header__title', activeTab.code === TABS.MYINFOR.code ? 'active' : 'inactive')}
+          >
+            My Info
+          </button>
+          <button
+            onClick={() => setActiveTab(TABS.CHANGEPASSWORD)}
+            className={cx('header__title', activeTab.code === TABS.CHANGEPASSWORD.code ? 'active' : 'inactive')}
+          >
+            Change password
+          </button>
+        </div>
       </div>
-      <div className={cx('content')}>
-        <div className={cx('title')}>My Info</div>
-        <form onSubmit={handleSubmit(onSubmit)} className={cx('box')}>
-          <div className={cx('div1')}>
-            <div className={cx('avatar')}>{userInfo?.fullName && userInfo?.fullName[0]?.toUpperCase()}</div>
-          </div>
-          <div className={cx('div2')}>
-            <div className={cx('label')}>Nickname</div>
-            <input type='text' {...register('fullName')} defaultValue={userInfo?.fullName}></input>
-            <div className={cx('note')}>Nicknames can only be changed 30 days after initial setting or change.</div>
-            <div className={cx('note')}>You cannot change your nickname if there is an ongoing transaction.</div>
-            <div className={cx('note')}>
-              Only Korean/English letters/numbers can be used, and the same string as the email ID cannot be used.
+
+      {activeTab.code === TABS.MYINFOR.code && (
+        <div className={cx('content')}>
+          <div className={cx('title')}>My Info</div>
+          <form onSubmit={handleSubmit(onSubmit)} className={cx('box')}>
+            <div className={cx('div1')}>
+              <div className={cx('avatar')}>{userInfo?.fullName && userInfo?.fullName[0]?.toUpperCase()}</div>
             </div>
-            <div className={cx('label')}>Email</div>
-            <input type='text' {...register('email')} defaultValue={userInfo?.email}></input>
-            <div className={cx('note')}>To change your email address, please contact customer service.</div>
-            <div className={cx('label')}>Phone number</div>
-            <input type='number' {...register('phoneNumber')} defaultValue={userInfo?.phoneNumber}></input>
-            <div className={cx('note')}>Please complete authentication to use the service smoothly.</div>
-            <button
-              onClick={() => {
-                formInput.current.click()
-              }}
-              type='submit'
-              disabled={isUpdating}
-            >
-              Save
-            </button>
-          </div>
-        </form>
-      </div>
+            <div className={cx('div2')}>
+              <div className={cx('label')}>Nickname</div>
+              <input type='text' {...register('fullName')} defaultValue={userInfo?.fullName}></input>
+              <div className={cx('note')}>Nicknames can only be changed 30 days after initial setting or change.</div>
+              <div className={cx('note')}>You cannot change your nickname if there is an ongoing transaction.</div>
+              <div className={cx('note')}>
+                Only Korean/English letters/numbers can be used, and the same string as the email ID cannot be used.
+              </div>
+              <div className={cx('label')}>Email</div>
+              <input type='text' {...register('email')} defaultValue={userInfo?.email}></input>
+              <div className={cx('note')}>To change your email address, please contact customer service.</div>
+              <div className={cx('label')}>Phone number</div>
+              <input type='number' {...register('phoneNumber')} defaultValue={userInfo?.phoneNumber}></input>
+              <div className={cx('note')}>Please complete authentication to use the service smoothly.</div>
+              <button
+                onClick={() => {
+                  formInput.current.click()
+                }}
+                type='submit'
+                disabled={isUpdating}
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+      {activeTab.code === TABS.CHANGEPASSWORD.code && (
+        <div className={cx('content')}>
+          <div className={cx('title')}>Change password</div>
+        </div>
+      )}
     </div>
   )
 }
